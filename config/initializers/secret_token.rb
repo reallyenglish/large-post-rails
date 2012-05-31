459433ec8e31893bfe6e8cf9d97f306a6fa7281b
Tatsuya Ono <ononoma@gmail.com>
2012-05-31 12:48:43 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LargePostRails::Application.config.secret_token = '7147b1c71ce4663679600fd584813b7d5a7d61a4419f0b33155dbbda2ec1ddbc0606636cdb41a5316c55c47392c6f057cff1a4465ae8590f8eb7a67519ade315'
